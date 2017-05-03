cbd3d92d8944269c0079e18446af9ca59f6c8d10
stefanmiljakovic <stefanmiljakovic@yahoo.com>
2017-05-03 13:29:37 +0200

    Add SetViewHolder with itemViews

--- app/src/main/java/com/week1/practice1/budgetmanager/MainActivity.java
+++ app/src/main/java/com/week1/practice1/budgetmanager/MainActivity.java
@@ -1,30 +1,53 @@
 package com.week1.practice1.budgetmanager;
 
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.LinearLayout;
+
+import com.week1.practice1.budgetmanager.data.dataContractDbHelper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    public SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Main Activity"); // Main Acitivty name
 
-        FloatingActionButton fbtnMain = (FloatingActionButton)findViewById(R.id.floatBtnMain);
-
-        fbtnMain.setOnClickListener(new View.OnClickListener() {
+        // Floating Button
+        FloatingActionButton fbMain = (FloatingActionButton)findViewById(R.id.floatBtnMain);
+        fbMain.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToAdd();
             }
         });
+
+        //Recycler View
+        LinearLayoutManager ll = new LinearLayoutManager(this);
+        recyclerView = (RecyclerView)findViewById(R.id.rv);
+        recyclerView.setLayoutManager(ll);
+
+        dataContractDbHelper DBHelper = new dataContractDbHelper(this);
+        SQLiteDatabase mDb = DBHelper.getWritableDatabase();
+    }
+
+    public final SQLiteDatabase getWritableDatabase() {
+        return mDb;
     }
 
     private void goToAdd(){
---
+++ app/src/main/java/com/week1/practice1/budgetmanager/adapter/MainAdapter.java
@@ -0,0 +1,26 @@
+package com.week1.practice1.budgetmanager.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+/**
+ * Created by dev7 on 3.5.17..
+ */
+
+public class MainAdapter extends RecyclerView.Adapter<SetViewHolder>{
+
+    @Override
+    public SetViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    }
+
+    @Override
+    public void onBindViewHolder(SetViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
